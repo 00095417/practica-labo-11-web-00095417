3599be957e3032c9fcb4fac486c8f616c3061923
meight10 <00267016@uca.edu.sv>
2018-11-16 08:31:23 -0600

    trabajando con php

---
+++ models/Task.php
@@ -0,0 +1,29 @@
+<?php
+//con la palabra reservada class creamos una clase en php
+class Task
+{
+  private $id;
+  private $task;
+  private $dateTask;
+  /*Metodos setter*/
+  public function setId($id){
+    $this->id = $id;
+  }
+  public function setTask($task){
+    $this->task=$task;
+  }
+  public function setDateTask($dateTask){
+    $this->dateTask = $dateTask;
+  }
+  /*Metodos setter*/
+  public function getId(){
+    return $this->id;
+  }
+  public function getTask(){
+    return $this->task;
+  }
+  public function getDateTask(){
+    return $this->dateTask;
+  }
+}
+?>
